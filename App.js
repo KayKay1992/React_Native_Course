c21d82c5395758e4036c3d4bc70295d808513e10
KayKay1992 <kennethnwankpa1992@gmail.com>
2024-11-04 12:24:24 -0800

    Exploring Layout

--- App.js
+++ App.js
@@ -1,35 +1,25 @@
 
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.appContainer}>
       <View>
-        <Text style={{margin: 16, borderWidth: 1, borderColor: 'red', padding: 16}}>Welcome to React Native!</Text>
-        <Text style={styles.dummyText}>This is an example app.</Text>
-        <View>
-        <Text style={styles.dummyText}>when you press the button nothing happens </Text>
-          <Text> because we have not added interactivity</Text>   
-        </View>
-        
+        <TextInput placeholder='Your Course Goal'/>
+        <Button title='Add Goal' />
       </View>
-      <Text>Hello world</Text>
-      <Button title="Click me"/>
+      <View>
+      
+        {/* Display goals here */}
+        <Text>Your Goals:</Text>
+      </View>
+     
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  dummyText: {
-    marginTop: 16,
-    borderWidth: 1,
-    borderColor: 'red',
-    padding: 16,
-  }
+ appContainer: {
+  padding: 50,
+ },
 });
