0983887f0c9cbf3d90a2d96bdc04cd87ab301f0e
KayKay1992 <kennethnwankpa1992@gmail.com>
2024-11-04 06:49:09 -0800

    Understanding Core Components

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Welcome to React Native!</Text>
+        <Text>This is an example app.</Text>
+        <View>
+        <Text>when you press the button nothing happens </Text>
+          <Text> because we have not added interactivity</Text>   
+        </View>
+        
+      </View>
+      <Text>Hello world</Text>
+      <Button title="Click me"/>
     </View>
   );
 }
