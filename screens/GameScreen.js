bcd78dfcd6b2c3cc1f6f559be990e513cfa00b6a
KayKay1992 <kennethnwankpa1992@gmail.com>
2024-11-14 08:40:02 -0800

    Start Work on GameScreen: Building the structure

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,8 +1,26 @@
-import { Text } from "react-native";
+import {View,  Text, StyleSheet } from "react-native";
 function GameScreen() {
   return (
-    <Text>GameScreen</Text>
+    <View style={styles.GameScreen}>
+    <Text>Opponent's Guess</Text>
+       {/* Your Guess */}
+      <View>
+        <Text>Higher or Lower?</Text>
+        {/* +
+        - */}
+      </View>
+      <View>
+        {/* LOG ROUNDS */}
+      </View>
+    </View>
   )
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+    GameScreen: {
+        flex: 1,
+        padding: 20,
+    },
+})
