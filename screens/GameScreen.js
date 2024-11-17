4a8fbc0151922b80faea83be5517e16b2bf2344c
KayKay1992 <kennethnwankpa1992@gmail.com>
2024-11-17 03:38:43 -0800

    Working with icons i.e Adding icons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import {View, StyleSheet, Alert} from "react-native";
+import Ionicons from '@expo/vector-icons/Ionicons';
 import Title from "../components/Title";
 import NumberContainer from "../components/Game/NumberContainer";
 import PrimaryButton from "../components/PrimaryButton";
 import Card from "../components/ui/Card";
 import InstructionText from "../components/ui/InstructionText";
+
 
 // GameScreen.js
 
@@ -56,10 +58,10 @@ function GameScreen({userNumber, onGameOver}) {
         <InstructionText style={styles.InstructionText}>Higher or Lower?</InstructionText>
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
-          <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}><Ionicons name="add" size={24} color="white" /></PrimaryButton>
           </View>
         <View style={styles.buttonContainer}>
-        <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+        <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}><Ionicons name="remove" size={24} color="white" /></PrimaryButton>
         </View>
         </View>
       </Card>
