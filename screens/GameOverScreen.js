c2b5ae68077dba08b75b46bdea0534492a0dff50
KayKay1992 <kennethnwankpa1992@gmail.com>
2024-11-19 01:29:48 -0800

    Adding a foreground Image, ignore the modification in package json and package-lock

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,8 +1,44 @@
-import { Text } from "react-native";
+import { Image, View, StyleSheet, Text } from "react-native";
+import Title from "../components/Title";
+import Colors from "../constants/colors";
 
 
 export default function GameOverScreen() {
   return (
-    <Text>GameOverScreen</Text>
+    <View style={styles.rootContainer}>
+      <Title> Game Over !!</Title>
+      <View style={styles.imageContainer}>
+      <Image style={styles.imageStyle} source={require('../assets/images/success.jpg')}/>
+      </View>  
+      <Text>Your Phone needed x rounds to guess the number y.</Text>
+    </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  // Add your styles here
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary700,
+    overflow: 'hidden',
+    margin: 36,
+  },
+  imageStyle: {
+    width: '100%',
+    height: '100%',
+  },
+
+
+});
