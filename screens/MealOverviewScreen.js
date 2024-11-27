49d99f81e49785cd4c2623a43a6d0daff883d6dd
KayKay1992 <kennethnwankpa1992@gmail.com>
2024-11-26 17:47:06 -0800

    Displaying the meals title

--- screens/MealOverviewScreen.js
+++ screens/MealOverviewScreen.js
@@ -1,4 +1,6 @@
-import { Text, View, StyleSheet } from "react-native";
+import { FlatList, View, StyleSheet , Text} from "react-native";
+import MealItem from "../components/MealItem";
+import { MEALS } from "../data/dummy-data";
 // import { useRoute } from '@react-navigation/native'
 
 
@@ -6,9 +8,19 @@ function MealOverviewScreen({route}) {
     // const route = useRoute()
 //   const catId =  route.params.categoryId
    const catId = route.params.categoryId
+   const displayedmeals = MEALS.filter((mealItem)=>{
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+   });
+
+   function renderMealItem(itemData){
+    return (
+       <MealItem title={itemData.item.title}/>
+    )
+   }
+
     return (
    <View style={styles.container}>
-    <Text>Meal Overview Screen - {catId}</Text>
+    <FlatList data={displayedmeals} keyExtractor={(item) => item.id} renderItem={renderMealItem}/>
    </View>
     );
 }
