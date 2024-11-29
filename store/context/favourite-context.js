42131cb1d98de1501e06ce14e0516285e5549f14
KayKay1992 <kennethnwankpa1992@gmail.com>
2024-11-29 15:40:08 -0800

    Adding Logic to our context

--- store/context/favourite-context.js
+++ store/context/favourite-context.js
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 // Create a new context for the meals data
 
@@ -10,8 +10,27 @@ export const FavouriteContext = createContext({
 })
 
 function FavouritesContextProvider({children}) {
+    const [favouriteMealIds, setFavouriteMealIds] = useState([]);
+    
+    const addFavourite = (id) => {
+        setFavouriteMealIds((currentFavIds) => [...currentFavIds, id]);
+    }
+    
+    const removeFavourite = (id) => {
+        setFavouriteMealIds((currentFavIds) => currentFavIds.filter((mealId) => mealId!== id));
+    }
+    
+    const clearFavourite = () => {
+        setFavouriteMealIds([]);
+    }
+    const value = {
+        ids: favouriteMealIds,
+        addFavourite : addFavourite,
+        removeFavourite : removeFavourite,
+        clearFavourites : clearFavourite,
+     };
     return (
-        <FavouriteContext.Provider >
+        <FavouriteContext.Provider value={value} >
             {children}
         </FavouriteContext.Provider>
     )
