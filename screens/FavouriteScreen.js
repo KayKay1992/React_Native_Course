8cf20c048edfc4f4e667b6f76ea2a0bc54c894da
KayKay1992 <kennethnwankpa1992@gmail.com>
2024-11-30 07:37:39 -0800

    Managing Favourite meal with the context API... displaying favourite meal in favourite screen

--- screens/FavouriteScreen.js
+++ screens/FavouriteScreen.js
@@ -1,8 +1,39 @@
+import { useContext } from 'react'
+import { StyleSheet , View, Text} from 'react-native';
+import MealFavouriteList from '../components/MealFavouriteList'
+import { FavouriteContext } from '../store/context/favourite-context';
+import { MEALS } from '../data/dummy-data';
 
-import { Text } from 'react-native'
 
 export default function FavouriteScreen() {
+  // Fetch or retrieve the favourite meals from the FavouriteContext
+ const favouriteMealContext =  useContext(FavouriteContext);
+
+ const favouriteMeals = MEALS.filter((meal) => favouriteMealContext.ids.includes(meal.id));
+
+ if(favouriteMeals.length === 0){
   return (
-    <Text>FavouriteScreen</Text>
+   <View style={styles.rootContainer}>
+    <Text style={styles.text}>No favourite meals found</Text>
+   </View>
+  )
+ }
+  return (
+   <MealFavouriteList items={favouriteMeals}/>
   )
 }
+
+
+const styles = StyleSheet.create({
+    rootContainer: {
+      flex: 1,
+      padding: 20,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    text: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: 'white',
+    },
+})
--- screens/MealOverviewScreen.js
+++ screens/MealOverviewScreen.js
@@ -1,7 +1,6 @@
 import {useLayoutEffect} from 'react'
-import { FlatList, View, StyleSheet , Text} from "react-native";
-import MealItem from "../components/MealItem";
 import { MEALS, CATEGORIES } from "../data/dummy-data";
+import MealFavouriteList from '../components/MealFavouriteList';
 // import { useRoute } from '@react-navigation/native'
 
 
@@ -19,48 +18,8 @@ function MealOverviewScreen({route, navigation}) {
     navigation.setOptions({
       title: categoryTItle
     })
-   }, [catId, navigation]) // Only re-run effect if catId changes
+   }, [catId, navigation])
 
-
-   function renderMealItem(itemData){
-    const item = itemData.item;
-    const mealItemProps = {
-        id: item.id,
-        title: item.title,
-        imageUrl: item.imageUrl,
-        duration: item.duration,
-        complexity: item.complexity,
-        affordability: item.affordability,
-        // isGlutenFree: item.isGlutenFree,
-        // isVegan: item.isVegan,
-        // isVegetarian: item.isVegetarian,
-        // isLactoseFree: item.isLactoseFree,
-        // isSustainable: item.isSustainable,
-        // isDairyFree: item.isDairyFree,
-        // isNutFree: item.isNutFree,
-        // isSugarFree: item.isSugarFree,
-        // isPeanutFree: item.isPeanutFree,
-        // isFishFree: item.isFishFree,
-        // isShellfishFree: item.isShellfishFree,
-    }
-    return (
-       <MealItem {...mealItemProps}/>
-    )
-   }
-
-    return (
-   <View style={styles.container}>
-    <FlatList data={displayedmeals} keyExtractor={(item) => item.id} renderItem={renderMealItem}/>
-   </View>
-    );
-}
-
-export default MealOverviewScreen;
-
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 16,
-    }
-})
+   return <MealFavouriteList items={displayedmeals}/>
+};
+       export default MealOverviewScreen;
