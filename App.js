b58a4e24f5483a2b5fb2ee77640df557b4863eb3
KayKay1992 <kennethnwankpa1992@gmail.com>
2024-12-06 15:59:33 -0800

    Adding Global colors and editing Navigation Configuration

--- App.js
+++ App.js
@@ -2,18 +2,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Ionicons} from "@expo/vector-icons"
 import ManageExpenses from './screens/ManageExpenses';
 import RecentExpenses from './screens/RecentExpenses';
 import AllExpenses from './screens/AllExpenses';
+import { GlobalStyles } from './constants/styles';
 
 const Stack = createNativeStackNavigator()
 const BottomTab = createBottomTabNavigator();
 
 function ExpensesTab() {
   return (
-    <BottomTab.Navigator>
-      <BottomTab.Screen name="RecentExpenses" component={RecentExpenses} />
-      <BottomTab.Screen name="AllExpenses" component={AllExpenses} />
+    <BottomTab.Navigator screenOptions={{
+      headerStyle: {backgroundColor: GlobalStyles.colors.primary500},
+      headerTintColor: 'white',
+      tabBarStyle: {backgroundColor: GlobalStyles.colors.primary500},
+      tabBarActiveTintColor: GlobalStyles.colors.accent500,
+      tabBarInactiveTintColor: GlobalStyles.colors.gray400,
+    }}>
+      <BottomTab.Screen name="RecentExpenses" component={RecentExpenses}
+       options={{
+        title: "Recent Expenses",
+        tabBarLabel: "Recent ",
+        tabBarIcon: ({color, size}) => <Ionicons name='hourglass' size={size} color={color}/>
+      }}
+       />
+      <BottomTab.Screen name="AllExpenses" component={AllExpenses} options={{
+        title: "All Expenses",
+        tabBarLabel: "All Expenses",
+        tabBarIcon: ({color, size}) => <Ionicons name='calendar' size={size} color={color}/>
+      }}
+       />
    
     </BottomTab.Navigator>
   )
@@ -25,13 +44,13 @@ export default function App() {
       <StatusBar style="auto" />
       <NavigationContainer>
         <Stack.Navigator >
-        <Stack.Screen name="ExpensesTab" component={ExpensesTab} />
+        <Stack.Screen name="ExpensesTab" component={ExpensesTab} options={{headerShown: false}}/>
           <Stack.Screen name="ManageExpenses" component={ManageExpenses} />
         </Stack.Navigator>
       </NavigationContainer>
       
   </>
-  ) ;
+  ) 
 }
 
 
---
+++ constants/styles.js
@@ -0,0 +1,21 @@
+export const GlobalStyles = {
+   colors :{primary50: '#e4d9fd',
+    primary100: '#c6affc',
+    primary200: '#a281f0',
+    primary400: '#5721d4',
+    primary400: '#3e04c3',
+    primary500: '#2d0689',
+    primary700: '#18043d',
+    primary800: '#0f032f',
+    accent500: '#f7bc0c',
+    accent600: '#e08d05',
+    accent700: '#c56000',
+    accent800: '#9b3b00',
+    accent900: '#712500',
+    error50: '#fcc4e4',
+    error500: '#c91e5a',
+    gray500: '#39324a',
+    gray700: '#221c30',
+   } 
+}
+   
