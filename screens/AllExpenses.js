322481b23a9a20369ec9c53cf74abd753e86684e
KayKay1992 <kennethnwankpa1992@gmail.com>
2024-12-06 09:43:04 -0800

    Adding Our screens and installing bottom-tabs navigation package

---
+++ screens/AllExpenses.js
@@ -0,0 +1,8 @@
+
+import { Text } from 'react-native'
+
+export default function AllExpenses() {
+  return (
+    <Text>AllExpenses</Text>
+  )
+}
---
+++ screens/ManageExpenses.js
@@ -0,0 +1,8 @@
+
+import { Text } from 'react-native'
+
+export default function ManageExpenses() {
+  return (
+    <Text>AllExpenses</Text>
+  )
+}
