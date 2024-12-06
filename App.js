a006bf325a48a3b16c7ab2b162a8ae9279054ce9
KayKay1992 <kennethnwankpa1992@gmail.com>
2024-12-06 09:30:05 -0800

    Clearing our Project for new project EXpense Tracker: Deleting the files we dont need

--- App.js
+++ App.js
@@ -1,78 +1,13 @@
 
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import {Ionicons} from '@expo/vector-icons'
-import MealDetailScreen from './screens/MealDetailScreen';
-import CategoriesScreen from './screens/CategoriesScreen';
-import MealOverviewScreen from './screens/MealOverviewScreen';
-import FavouriteScreen from './screens/FavouriteScreen';
-// import FavouritesContextProvider from './store/context/favourite-context';
-import { store } from './store/redux/store';
-import { Provider } from 'react-redux';
 
-//make sure you install the compatible version of react-navigation/native-stack to avoid error
-
-const Stack = createNativeStackNavigator();
-
-const Drawer = createDrawerNavigator();
-
-function DrawerNavigator(){
-   // Here you can create your own custom drawer navigator
- 
-  return (
-    <Drawer.Navigator screenOptions={{
-      headerStyle: { backgroundColor: '#351401'},
-      headerTintColor: 'white',
-      sceneStyle: { backgroundColor: '#3f2f01' },
-      drawerContentStyle: { backgroundColor: '#351401'},
-      drawerInactiveTintColor: 'white',
-      drawerActiveTintColor: 'orange',
-      drawerActiveBackgroundColor: '#3f2f01',
-    }}
-      >
-      <Drawer.Screen name='Categeries' component={CategoriesScreen} options={{
-        title: 'All Categories',
-        drawerIcon: ({ size, color }) => (
-          <Ionicons name="list-circle-sharp" size={size} color={color} />
-        )
-      }}/>
-      <Drawer.Screen name='Favourite' component={FavouriteScreen} options={{
-         drawerIcon: ({ size, color }) => (
-          <Ionicons name="heart" size={size} color={color} />
-        )
-      }}/>
-    </Drawer.Navigator>
-  );
-
-}
 export default function App() {
   return(
-  <>
-       <StatusBar style='light'/>
-       <Provider store={store}>
-       {/* <FavouritesContextProvider> */}
-       <NavigationContainer>
-         <Stack.Navigator screenOptions= {{
-            headerStyle: { backgroundColor: '#351401'},
-            headerTintColor: 'white',
-            contentStyle: { backgroundColor: '#3f2f01' },
-            headerStyle: { backgroundColor: '#351401'},
-         }}>
-          <Stack.Screen name="Drawer" component={DrawerNavigator} options={{
-            headerShown: false,
-          }} />
-          <Stack.Screen name="MealOverview" component={MealOverviewScreen}  
-           />
-           <Stack.Screen name="MealDetail" component={MealDetailScreen} 
-           />
-        </Stack.Navigator> 
-     </NavigationContainer>
-     {/* </FavouritesContextProvider> */}
-     </Provider>
-    </>
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text>Hello, React Native!</Text>
+    </View>
   ) ;
 }
 
