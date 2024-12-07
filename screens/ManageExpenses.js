0b0ac5a5e4123ad17fc5830544403471417f5b40
KayKay1992 <kennethnwankpa1992@gmail.com>
2024-12-07 06:50:15 -0800

    Adding Delete Button

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,6 +1,8 @@
 
 import { useLayoutEffect } from 'react';
-import { Text } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+import IconButton from '../components/UI/iconButton';
+import { GlobalStyles } from '../constants/styles';
 
 export default function ManageExpenses({route, navigation}) {
   const edittedExpenseId = route.params?.expenseId;
@@ -13,10 +15,36 @@ export default function ManageExpenses({route, navigation}) {
       title: isEditing ? 'Edit Expense' : 'Add Expense'
     }, [navigation, isEditing])
   })
+  
+  function deleteExpenseHandler(){
 
+  }
  
 
   return (
-    <Text>AllExpenses</Text>
+    <View style={styles.container}>
+      {/* Adding buttons closing d modals without editing, closing the modal after editing and deleting */}
+      {isEditing &&
+      <View style={styles.deleteContainer}>
+       <IconButton icon='trash' color={GlobalStyles.colors.error500} size={36} onPress={deleteExpenseHandler}/>
+       </View>}
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+   
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderColor: GlobalStyles.colors.primary200,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
